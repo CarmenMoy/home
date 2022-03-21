b0d4bdb228acf3174d2683c8f48e1aa6241e53ee
CarmenMoy <mariadelcarmenmoyaher@gmail.com>
2022-03-21 19:43:35 +0100

    cambios 21.03

--- js/app-mobile.js
+++ js/app-mobile.js
@@ -1,5 +1,8 @@
+// con este comando, importamos la variable creada en la carpeta de java mobile en documento menu y 
+// denominada "renderMenu".
+// Así con cada comando y documento de cada carpeta...
 import {renderMenu} from './mobile/menu.js';
-import {renderTabs} from './mobile/tabs.js';
 
+// A su vez de darle importar (import), debemos detallar variable y "()", para que lo importe todo.
+// Todo su contenido.
 renderMenu();
-renderTabs();
--- js/mobile/menu.js
+++ js/mobile/menu.js
@@ -1,8 +1,21 @@
+// Le aplicamos export para poder importarlo al documneto conjusto de java en este caso, app-mobile.
+// y le aplicamos el nombre en este caso, renderMenu (que volveremos a meter en app-mobile).
+// Esto por cada variable o conjunto de variables generadas.
 export let renderMenu = () => {
 
+    // let, indica que vamos a aplicarle una variable a un elemento (div) de html. Generamos una
+    // variable a la cual le daremos eventos (acciones).
+
+    // En este caso, a la variable, le denominamos, menuButton para identificar la acción del botón.
+    // Nuestro botón o hamburguesa (en este caso) denominado en html "collapse-button".
     let menuButton = document.getElementById("collapse-button");
+    // Además, le damos una variable al menú, que se active y desactive con el button.
     let menu = document.getElementById("menu");
 
+    // Esto es el evento de la variable (let) que hemos denominado "menuButton" ya que se lo aplica-
+    // mos al botón (hamburguesa) que tenemos en el menú de nuestra cabecera html.
+    // El evento es que al hacer click con el ratón, se active la acción. En este caso, se activará
+    // o aparecerá el menú a la vez que la hamburguesa se transforma en una x (ver css/scss).
     menuButton.addEventListener("click", () => {
         menuButton.classList.toggle("active");
         menu.classList.toggle("active");
